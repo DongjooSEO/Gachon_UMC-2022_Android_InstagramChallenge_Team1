19e7374c7cd903cd7cdb4d7f34638bc0a9ae30cf
Seoljingni <gina030212@gmail.com>
2022-06-12 23:34:01 +0900

    First

--- app/src/main/java/com/example/InstagramClone/SearchFragment.kt
+++ app/src/main/java/com/example/InstagramClone/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.example.InstagramClone
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.InstagramClone.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment() {
 
+class SearchFragment : Fragment() {
     lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
@@ -17,6 +18,10 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        binding.searchBarTv.setOnClickListener {
+            startActivity(Intent(activity, SearchingFragment::class.java))
+        }
 
         return binding.root
     }
